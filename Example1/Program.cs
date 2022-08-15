6db301032e98518b20240c1d14735293597b627c
kadzuki <heaven.roma@mail.ru>
2022-08-15 21:29:42 +0300

    Задача41

---
+++ Example1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите числа");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > count)
+    {
+      sum ++;
+    }
+}
+Console.WriteLine($"Чисел больше нуля: {sum}");
+
